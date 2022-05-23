f0e176ef275ea6bc1b99887c8ed6bcb21358cf74
siddiq <asiddiq538@gmail.com>
2022-05-23 06:50:02 +0530

    new files

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ function Header(){
         <div className="App">
             <nav id="scrollspy" className="navbar navbar-light bg-light navbar-expand-lg fixed-top" data-spy="affix" data-offset-top="20">
               <div className="container">
-                  <a className="navbar-brand" href="#"><img src="assets/imgs/brand.svg" alt="" className="brand-img" /></a>
+                  <a className="navbar-brand" href="#"></a>
                   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span className="navbar-toggler-icon"></span>
                   </button>
